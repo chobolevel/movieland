7d2780f54cdaefa7c99b5bbef5508c07075326a1
chobolevel <rodaka123@naver.com>
2023-09-22 03:29:06 +0900

    Feat: project init

---
+++ src/main/java/kr/co/movieland/MovielandApplication.java
@@ -0,0 +1,13 @@
+package kr.co.movieland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielandApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MovielandApplication.class, args);
+  }
+
+}
