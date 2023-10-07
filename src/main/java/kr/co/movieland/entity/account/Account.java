e50928828a107e3cbd32461dcd43c0eae82a137c
chobolevel <rodaka123@naver.com>
2023-10-07 14:57:15 +0900

    Refactor: modify Account join
    회원 정보 조회 시 예매 정보 가져올 수 있도록 수정

--- src/main/java/kr/co/movieland/entity/account/Account.java
+++ src/main/java/kr/co/movieland/entity/account/Account.java
@@ -2,8 +2,11 @@ package kr.co.movieland.entity.account;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import kr.co.movieland.entity.common.BaseEntity;
+import kr.co.movieland.entity.movie.MovieTicket;
 import kr.co.movieland.enums.Account.AccountRoleType;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,5 +26,7 @@ public class Account extends BaseEntity {
   private String resignYn;
 
   private String authNumber;
+
+  private List<MovieTicket> movieTicketList;
 
 }
